6f7fbad35a1ac4c5b2fda78068b03d035fc50ca9
Irina Rogova <buyersbirds@gmail.com>
2020-11-03 21:12:29 +0600

    Initinal commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int ticket = 12000;
+        int miles = 20 * 1;
+        int totalmiles = ticket / miles;
+        return totalmiles;
+    }
+
+}
